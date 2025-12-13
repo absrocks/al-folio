c9852d384082452402438b0d9742c887f8b737b3
absrocks <65731510+absrocks@users.noreply.github.com>
2025-12-13 20:57:33 +0000

    Deploying to gh-pages from @ absrocks/al-folio@0df8ad00a9b3adf1fb5d93c2384b13b9f2e78084 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -31,8 +31,8 @@ ninja.data = [{
             window.location.href = "/al-folio/projects/";
           },
         },{id: "nav-repositories",
-          title: "repositories",
-          description: "Edit the `_data/repositories.yml` and change the `github_users` and `github_repos` lists to include your own GitHub profile and repositories.",
+          title: "Repositories",
+          description: "See the list of repositories contributed by Abhishek",
           section: "Navigation",
           handler: () => {
             window.location.href = "/al-folio/repositories/";
